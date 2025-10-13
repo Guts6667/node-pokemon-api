4e1bd46925a8da121caf0f150888755ae4a571f8
Rayan <rayanchambet1@gmail.com>
2025-10-13 16:54:16 +0700

    Refactors application structure using Sequelize
    Restructures the application to use Sequelize ORM for database interactions.
    
    This change introduces a new database initialization module and separates route handling into individual files for better organization and maintainability. It also removes the mock data and utility functions.
    
    This change simplifies database interactions and improves code structure.

--- app.js
+++ app.js
@@ -1,98 +1,25 @@
 const express = require("express");
-const { Sequelize, DataTypes } = require("sequelize");
-const { sucess, getUniqueId } = require("./src/utils/helper");
+const sequelize = require("./src/db/sequelize");
 const morgan = require("morgan");
 const favicon = require("serve-favicon");
-const pokemonModel = require("./src/models/pokemons");
-
-let pokemons = require("./src/db/mock-pokemon");
 
 const app = express();
 
 const port = 3000;
-
-const sequelize = new Sequelize("pokedex", "root", "root", {
-  host: "localhost",
-  port: 8889,
-  dialect: "mariadb",
-  dialectOptions: {
-    timezone: "Etc/GMT-2",
-  },
-  logging: false,
-});
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
-
-const pokemon = pokemonModel(sequelize, DataTypes);
-
-sequelize.sync({ force: false }).then(() => {
-  console.log("All models were synchronized successfully.");
-
-  pokemons.map((item) => {
-    pokemon
-      .create({
-        name: item.name,
-        hp: item.hp,
-        cp: item.cp,
-        picture: item.picture,
-        types: item.types.join(),
-      })
-      .then((newPokemon) => {
-        console.log(newPokemon.toJSON());
-      });
-  });
-});
-
-app.get("/", (req, res) => res.send("Hello Express 🥳 2!"));
 
 app
   .use(favicon(__dirname + "/favicon.ico"))
   .use(morgan("dev"))
   .use(express.json());
 
-app.get("/api/pokemons", (req, res) => {
-  const message = `There is ${pokemons.length} pokemons in the pokedex at the moment`;
-  res.json(sucess(message, pokemons));
-});
-app.get("/api/pokemons/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const pokemon = pokemons.find((pokemon) => pokemon.id === id);
-  const message = "We found a Pokemon";
-  res.json(sucess(message, pokemon));
-});
+sequelize.initDb();
 
-app.post("/api/pokemons", (req, res) => {
-  const id = getUniqueId(pokemons);
-  const createdPokemon = { ...req.body, ...{ id, created: new Date() } };
-  pokemons.push(createdPokemon);
-  const message = `The pokemon ${createdPokemon.name} has been created successfully`;
-  res.json(sucess(message, createdPokemon));
-});
-
-app.put("/api/pokemons/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const updatedPokemon = { ...req.body, id, updated: new Date() };
-  pokemons = pokemons.map((pokemon) =>
-    pokemon.id === id ? updatedPokemon : pokemon
-  );
-  const message = `The pokemon ${updatedPokemon.name} has been updated successfully`;
-  res.json(sucess(message, updatedPokemon));
-});
-
-app.delete("/api/pokemons/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const deletedPokemon = pokemons.find((pokemon) => pokemon.id === id);
-  pokemons = pokemons.filter((pokemon) => pokemon.id !== id);
-  const message = `The pokemon ${deletedPokemon.name} has been deleted successfully`;
-  res.json(sucess(message, deletedPokemon));
-});
+// API's Endpoints
+require("./src/routes/findAllPokemons")(app);
+require("./src/routes/findPokemonByPk")(app);
+require("./src/routes/createPokemon")(app);
+require("./src/routes/updatePokemon")(app);
+require("./src/routes/deletePokemon")(app);
 
 app.listen(port, () =>
   console.log(`Node app started on: http://localhost:${port}`)
---
+++ src/db/sequelize.js
@@ -0,0 +1,35 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const PokemonModel = require("../models/pokemons");
+const pokemons = require("./mock-pokemon");
+
+const sequelize = new Sequelize("pokedex", "root", "root", {
+  host: "localhost",
+  port: 8889,
+  dialect: "mariadb",
+  dialectOptions: {
+    timezone: "Etc/GMT-2",
+  },
+  logging: false,
+});
+
+const Pokemon = PokemonModel(sequelize, DataTypes);
+
+const initDb = () => {
+  return sequelize.sync({ force: true }).then((_) => {
+    pokemons.map((pokemon) => {
+      Pokemon.create({
+        name: pokemon.name,
+        hp: pokemon.hp,
+        cp: pokemon.cp,
+        picture: pokemon.picture,
+        types: pokemon.types.join(),
+      }).then((pokemon) => console.log(pokemon.toJSON()));
+    });
+    console.log("Database initialized successfully!");
+  });
+};
+
+module.exports = {
+  initDb,
+  Pokemon,
+};
