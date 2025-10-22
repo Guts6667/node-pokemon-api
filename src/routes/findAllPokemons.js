da44aa3cb7b1efbc4b677003a517838856ecc1c0
Rayan <rayanchambet1@gmail.com>
2025-10-22 16:40:30 +0700

    Improves pokemon search and listing
    Enhances the pokemon listing API to support pagination
    and improves name-based searching by implementing limit,
    offset parameters as well as an ordering.
    
    Removes the name length validation to allow searches for names
    with less than 3 characters.

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -5,25 +5,32 @@ module.exports = (app) => {
   app.get("/api/pokemons", (req, res) => {
     if (req.query.name) {
       const name = req.query.name;
+      const limit = parseInt(req.query.limit) || 5;
+      const offset = parseInt(req.query.offset) || 0;
 
-      if (name.length < 3) {
-        const message = "The name must be at least 3 characters long.";
-        return res.status(400).json({ message }); // Bad request
-      }
+      // if (name.length < 2) {
+      //   const message = "The name must be at least 2 characters long.";
+      //   return res.status(400).json({ message }); // Bad request
+      // }
 
-      return Pokemon.findAll({
+      return Pokemon.findAndCountAll({
+        // Use findAndCountAll to get both the count and the rows
         where: {
           name: {
             // We use Op.like to perform a case-insensitive search
             [Op.like]: `%${name}%`, // The % wildcard allows us to search for names that contain the query string anywhere
           },
         },
-      }).then((pokemons) => {
-        const message = `La liste des pokémons avec le nom ${name} a bien été récupérée.`;
-        res.json({ message, data: pokemons });
+        order: ["name"], // Order the results by name
+        limit: limit, // Limit the number of results
+        offset: offset, // Skip the first 'offset' results
+      }).then(({ count, rows }) => {
+        // Destructure count and rows from the result
+        const message = `${count} pokémon(s) found matching the name "${name}".`;
+        res.json({ message, data: rows }); // Return only the rows (pokemons)
       });
     } else {
-      Pokemon.findAll()
+      Pokemon.findAll({ order: ["name"] }) // Order the results by name
         .then((pokemons) => {
           const message = "La liste des pokémons a bien été récupérée.";
           res.json({ message, data: pokemons });
